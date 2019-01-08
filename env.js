8949431d4b36884ae3b15ef8d703a20dfe7437c2
Jayant Shrivastava <jshrivastava03@gmail.com>
2019-01-08 16:37:19 -0500

    Heroku

---
+++ env.js
@@ -0,0 +1,6 @@
+module.exports = {
+  "db_host": "shopify-store-v2.cihxlzljrdyu.us-east-2.rds.amazonaws.com",
+  "db_name": "shopify2019",
+  "db_user": "admin",
+  "db_password": "8adsfa9sdfna9bfsdnnaansf"
+}
