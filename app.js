ca77f870e4b4715194cfa3ecf9f848838a19d109
Jayant Shrivastava <jshrivastava03@gmail.com>
2019-01-08 16:46:37 -0500

    Small Updates for Heroku

--- app.js
+++ app.js
@@ -8,12 +8,8 @@ const port = process.env.PORT || 5000;
 
 require('./routes')(app);
 
-// configure middleware
-// app.set('port', process.env.port || port); // set express to use this port
-app.set('views', __dirname + '/views'); // set express to look in this folder to render our view
-app.set('view engine', 'ejs'); // configure template engine
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json()); // parse form data client
+app.use(bodyParser.json()); 
 app.use(express.static(path.join(__dirname, 'public'))); // configure express to use public folder
 // app.use(fileUpload()); // configure fileupload
 
