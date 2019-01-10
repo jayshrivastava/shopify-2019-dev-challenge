0047801ed58363909667529fa98f98bd42433729
Jay <jshrivastava03@gmail.com>
2019-01-10 09:32:51 -0500

    Delete env.js

--- env.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  "db_host": "shopify-store-v2.cihxlzljrdyu.us-east-2.rds.amazonaws.com",
-  "db_name": "shopify2019",
-  "db_user": "admin",
-  "db_password": "8adsfa9sdfna9bfsdnnaansf"
-}
